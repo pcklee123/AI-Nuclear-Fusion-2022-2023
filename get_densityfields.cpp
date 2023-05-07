802b20eafaa16338397343587e7209f447388192
pcklee123 <pcklee123@gmail.com>
2023-05-07 17:32:24 +0800

    a

--- get_densityfields.cpp
+++ get_densityfields.cpp
@@ -12,7 +12,7 @@ void get_densityfields(float currentj[2][3][n_space_divz][n_space_divy][n_space_
     // find number of particle and current density fields
     // set limits beyond which particle is considered as "lost"
     static const float ddi[3] = {1.f / par->dd[0], 1.f / par->dd[1], 1.f / par->dd[2]}; // precalculate reciprocals
-    const float dti[2] = {1.f / par->dt[0], 1.f / par->dt[1]};
+    static const float dti[2] = {1.f / par->dt[0], 1.f / par->dt[1]};
     // cout << " par->dt[0]" << par->dt[0] << endl;
     // cell indices for each particle [2][3][n_parte]
     static auto *ii = static_cast<unsigned int(*)[3][n_parte]>(_aligned_malloc(2 * 3 * n_parte * sizeof(unsigned int), alignment));
@@ -20,19 +20,18 @@ void get_densityfields(float currentj[2][3][n_space_divz][n_space_divy][n_space_
     static auto *v = static_cast<float(*)[3][n_parte]>(_aligned_malloc(2 * 3 * n_parte * sizeof(float), alignment));
     // particle offsets array [2][3][n_parte]
     static auto *offset = static_cast<float(*)[3][n_parte]>(_aligned_malloc(2 * 3 * n_parte * sizeof(float), alignment));
-    // temp space
-    static auto *ftemp = static_cast<float(*)[n_space_divz][n_space_divy][n_space_divx]>(_aligned_malloc(8 * n_cells * sizeof(float), alignment));
+    // temp space 7 1. density e + i 2-7 current x,y,z p,n
+    static auto *ftemp = static_cast<float(*)[n_space_divz][n_space_divy][n_space_divx]>(_aligned_malloc(7 * n_cells * sizeof(float), alignment));
     // center of charge field arrays [2-particle type][3 pos][z][y][x]
     static auto *np_center = static_cast<float(*)[n_space_divz][n_space_divy][n_space_divx][3]>(_aligned_malloc(2 * 3 * n_cells * sizeof(float), alignment));
     // center of current field arrays [2][3-pos][3-current component][z][y][x]
     static auto *jc2 = static_cast<float(*)[2][3][n_space_divz][n_space_divy][n_space_divx]>(_aligned_malloc(2 * 2 * 3 * n_cells * sizeof(float), alignment));
     static auto *jc_center = static_cast<float(*)[2][3][n_space_divz][n_space_divy][n_space_divx][3]>(_aligned_malloc(2 * 2 * 3 * 3 * n_cells * sizeof(float), alignment));
-    //   static auto *jc_center = static_cast<float(*)[3][n_space_divz][n_space_divy][n_space_divx][3]>(_aligned_malloc(2 * 3 * 3 * n_space_divz * n_space_divy * n_space_divx * sizeof(float), alignment));
 
     // set fields=0 in preparation// Could split into threads
     fill(reinterpret_cast<float *>(np), reinterpret_cast<float *>(np) + n_cells, 0.f);
     //   fill(reinterpret_cast<float *>(currentj), reinterpret_cast<float *>(currentj) + n_cells * 2 * 3, 0.f);
-    // fill(reinterpret_cast<float *>(ftemp), reinterpret_cast<float *>(ftemp) + n_cells*8, 0.f);
+    fill(reinterpret_cast<float *>(ftemp), reinterpret_cast<float *>(ftemp) + n_cells * 7, 0.f);
     fill(reinterpret_cast<float *>(np_center), reinterpret_cast<float *>(np_center) + n_cells * 3 * 2, 0.f);
     fill(reinterpret_cast<float *>(jc2), reinterpret_cast<float *>(jc2) + n_cells * 2 * 2 * 3, 0.f);
     fill(reinterpret_cast<float *>(jc_center), reinterpret_cast<float *>(jc_center) + n_cells * 2 * 2 * 3 * 3, 0.f);
@@ -62,73 +61,42 @@ void get_densityfields(float currentj[2][3][n_space_divz][n_space_divy][n_space_
 #pragma omp parallel for simd
             for (unsigned int n = 0; n < par->n_part[p]; ++n)
             {
-                bool toolow = pos1x[p][n] <= par->posL_1[0], toohigh = pos1x[p][n] >= par->posH_1[0];
-                pos1x[p][n] = toolow ? par->posL_15[0] : (toohigh ? par->posH_15[0] : pos1x[p][n]);
-                pos0x[p][n] = (toolow || toohigh) ? pos1x[p][n] : pos0x[p][n];
+                bool toolow = pos1y[p][n] <= par->posL_1[1], toohigh = pos1y[p][n] >= par->posH_1[1];
+                pos1y[p][n] = toolow ? par->posL_15[1] : (toohigh ? par->posH_15[1] : pos1y[p][n]);
+                pos0y[p][n] = (toolow || toohigh) ? pos1y[p][n] : pos0y[p][n];
                 q[p][n] = (toolow || toohigh) ? 0 : q[p][n];
             }
 #pragma omp section
             for (unsigned int n = 0; n < par->n_part[p]; ++n)
             {
 #ifdef sphere
-                bool toolow = pos1x[p][n] <= par->posL_1[0], toohigh = pos1x[p][n] >= par->posH_1[0];
-                pos1x[p][n] = toolow ? par->posL_15[0] : (toohigh ? par->posH_15[0] : pos1x[p][n]);
-                pos0x[p][n] = (toolow || toohigh) ? pos1x[p][n] : pos0x[p][n];
+                bool toolow = pos1z[p][n] <= par->posL_1[2], toohigh = pos1z[p][n] >= par->posH_1[2];
+                pos1z[p][n] = toolow ? par->posL_15[2] : (toohigh ? par->posH_15[2] : pos1z[p][n]);
+                pos0z[p][n] = (toolow || toohigh) ? pos1z[p][n] : pos0z[p][n];
                 q[p][n] = (toolow || toohigh) ? 0 : q[p][n];
 #endif
 #ifdef cylinder // rollover particles in z direction
-                if (ii[p][2][n] == 0)
-                {
-                    pos1z[p][n] += (n_space_divz - 2) * par->dd[2];
-                    pos0z[p][n] += (n_space_divz - 2) * par->dd[2];
-                    nzm++;
-                }
-                else if (ii[p][2][n] >= n_space_divz - 1)
-                {
-                    pos1z[p][n] -= (n_space_divz - 2) * par->dd[2];
-                    pos0z[p][n] -= (n_space_divz - 2) * par->dd[2];
-                    nzp++;
-                }
+                bool toolow = pos1z[p][n] <= par->posL_1[2], toohigh = pos1z[p][n] >= par->posH_1[2];
+                pos1z[p][n] += toolow ? (n_space_divz - 2) * par->dd[2]; : (toohigh ? (2-n_space_divz ) * par->dd[2]; : 0);
+                pos0z[p][n] += toolow ? (n_space_divz - 2) * par->dd[2]; : (toohigh ? (2-n_space_divz ) * par->dd[2]; : 0);
 #endif
             }
         }
     }
 #pragma omp barrier
 
-#pragma omp parallel num_threads(2)
+#pragma omp parallel for num_threads(6)
+    for (int pc = 0; pc < 6; ++pc)
     {
-        int p = omp_get_thread_num();
-#pragma omp parallel sections
+        int p = pc / 3, c = pc % 3;
+#pragma omp parallel for simd
+        for (unsigned int n = 0; n < par->n_part[p]; ++n) // get cell indices (x,y,z) a particle belongs to
         {
-#pragma omp section
-            {
-#pragma omp parallel for simd
-                for (unsigned int n = 0; n < par->n_part[p]; ++n) // get cell indices (x,y,z) a particle belongs to
-                {
-                    ii[p][0][n] = (int)roundf((pos1x[p][n] - par->posL[0]) * ddi[0]);
-                    offset[p][0][n] = (pos1x[p][n] - par->posL[0]) * ddi[0] - (float)(ii[p][0][n]);
-                }
-            }
-#pragma omp section
-            {
-#pragma omp parallel for simd
-                for (unsigned int n = 0; n < par->n_part[p]; ++n) // get cell indices (x,y,z) a particle belongs to
-                {
-                    ii[p][1][n] = (int)roundf((pos1y[p][n] - par->posL[1]) * ddi[1]);
-                    offset[p][1][n] = (pos1y[p][n] - par->posL[1]) * ddi[1] - (float)(ii[p][1][n]);
-                }
-            }
-#pragma omp section
-            {
-#pragma omp parallel for simd
-                for (unsigned int n = 0; n < par->n_part[p]; ++n) // get cell indices (x,y,z) a particle belongs to
-                {
-                    ii[p][2][n] = (int)roundf((pos1z[p][n] - par->posL[2]) * ddi[2]);
-                    offset[p][2][n] = (pos1z[p][n] - par->posL[2]) * ddi[2] - (float)(ii[p][2][n]);
-                }
-            }
+            ii[p][c][n] = (int)roundf((pos1x[p][n] - par->posL[c]) * ddi[c]);
+            offset[p][c][n] = (pos1x[p][n] - par->posL[c]) * ddi[c] - (float)(ii[p][c][n]);
         }
     }
+
 #pragma omp barrier
     //  cout << "get_density_checked out of bounds\n";
 
@@ -188,6 +156,7 @@ void get_densityfields(float currentj[2][3][n_space_divz][n_space_divy][n_space_
             }
             smoothscalarfield(np[0], ftemp[0], np_center[0], 0);
             smoothscalarfield(np[1], ftemp[0], np_center[1], 1);
+            //npt is smoothed np[0] and np[1] are not
             memcpy(reinterpret_cast<float *>(npt), reinterpret_cast<float *>(ftemp[0]), n_cells * sizeof(float));
         }
 #pragma omp section
@@ -196,7 +165,7 @@ void get_densityfields(float currentj[2][3][n_space_divz][n_space_divy][n_space_
             for (int pc = 0; pc < 6; ++pc)
             {
                 int p = pc / 3, c = pc % 3;
-                int pc2 = pc + 2;
+                int pc2 = pc + 1;
                 int te = (pc + 1) * 2;
                 for (int n = 0; n < par->n_part[p]; ++n)
                 {
@@ -215,9 +184,9 @@ void get_densityfields(float currentj[2][3][n_space_divz][n_space_divy][n_space_
     }
 #pragma omp barrier
 
-//cout << "Max Np0" << maxvalf(reinterpret_cast<float *>(np[0]), n_cells) << endl;
-//cout << "Max Np1" << maxvalf(reinterpret_cast<float *>(np[1]), n_cells) << endl;
-//cout << "Max Npt" << maxvalf(reinterpret_cast<float *>(npt), n_cells) << endl;
+// cout << "Max Np0" << maxvalf(reinterpret_cast<float *>(np[0]), n_cells) << endl;
+// cout << "Max Np1" << maxvalf(reinterpret_cast<float *>(np[1]), n_cells) << endl;
+// cout << "Max Npt" << maxvalf(reinterpret_cast<float *>(npt), n_cells) << endl;
 #pragma omp parallel for simd num_threads(nthreads)
     for (unsigned int i = 0; i < n_cells * 3; i++)
         (reinterpret_cast<float *>(jc))[i] = (reinterpret_cast<float *>(currentj[0]))[i] + (reinterpret_cast<float *>(currentj[1]))[i];
--- smoothfield.cpp
+++ smoothfield.cpp
@@ -1,6 +1,6 @@
 #include "include/traj.h"
 // Interpolate the value at a given point
-void smoothscalarfield(float f[n_space_divz][n_space_divy][n_space_divx],float ftemp[n_space_divz][n_space_divy][n_space_divx],
+void smoothscalarfield(float f[n_space_divz][n_space_divy][n_space_divx], float ftemp[n_space_divz][n_space_divy][n_space_divx],
                        float fc[n_space_divz][n_space_divy][n_space_divx][3], int s)
 {
     float d;
@@ -9,13 +9,8 @@ void smoothscalarfield(float f[n_space_divz][n_space_divy][n_space_divx],float f
     else
         d = -0.00001f;
     // cout << "smoothfield" << endl;
- //    auto *ftemp = static_cast<float(*)[n_space_divy][n_space_divx]>(_aligned_malloc(n_space_divz * n_space_divy * n_space_divx * sizeof(float), alignment));
-
- //   auto *ftemp = static_cast<float(*)[n_space_divy][n_space_divx]>(_aligned_malloc(n_space_divz * n_space_divy * n_space_divx * sizeof(float), alignment));
-    // float ftemp[n_space_divz][n_space_divy][n_space_divx];
-    fill(reinterpret_cast<float *>(ftemp), reinterpret_cast<float *>(ftemp) + n_cells, 0.f);
     // calculate center of charge field as offsets (-0.5 to 0.5) from cell center
-    //   cout << "calculate center of charge field" << endl;
+    // cout << "calculate center of charge field" << endl;
     for (int k = 0; k < n_space_divz; ++k)
         for (int j = 0; j < n_space_divy; ++j)
             for (int i = 0; i < n_space_divx; ++i)
@@ -23,13 +18,8 @@ void smoothscalarfield(float f[n_space_divz][n_space_divy][n_space_divx],float f
                 fc[k][j][i][0] = (fc[k][j][i][0] / (f[k][j][i] + d));
                 fc[k][j][i][1] = (fc[k][j][i][1] / (f[k][j][i] + d));
                 fc[k][j][i][2] = (fc[k][j][i][2] / (f[k][j][i] + d));
-                //                if (f[k][j][i] != 0)
-                //                  cout << f[k][j][i] << " "
-                //                     << " " << i << " " << j << " " << k << endl;
-                if (fabs(fc[k][j][i][0]) > 0.5)
-                    cout << " (fabs(fc[k][j][i][0] )>0.5) " << d << " " << f[k][j][i] << " " << s << " " << i << " " << j << " " << k << endl;
             }
-      // calculate the 8 coefficients out of 27 and their indices
+    // calculate the 8 coefficients out of 27 and their indices
     /* center is [0][0][0] [dk][dj][di]so [-1][-1][-1],[-1][-1][0],[-1][-1][1] ... dk*n_space_divx*n_space_divx+dj*n_space_divx+di */
     int k1, j1, i1, sw;
     float fx0, fx1, fy0, fy1, fz0, fz1;
@@ -134,8 +124,6 @@ void smoothscalarfield(float f[n_space_divz][n_space_divy][n_space_divx],float f
                 default:
                     cout << "error smoothfield default " << sw << endl;
                 }
-                //       if (f[k0][j0][i0] != 0)
-                //             cout << ftemp[k0][j0][i0] << " ";
                 ftemp[k0][j0][i0] += f[k0][j0][i0] * fz1 * fy1 * fx1;
                 ftemp[k1][j0][i0] += f[k0][j0][i0] * fz0 * fy1 * fx1;
                 ftemp[k0][j1][i0] += f[k0][j0][i0] * fz1 * fy0 * fx1;
@@ -145,6 +133,4 @@ void smoothscalarfield(float f[n_space_divz][n_space_divy][n_space_divx],float f
                 ftemp[k0][j1][i1] += f[k0][j0][i0] * fz1 * fy0 * fx0;
                 ftemp[k1][j1][i1] += f[k0][j0][i0] * fz0 * fy0 * fx0;
             }
-    //     cout << "smoothfield copy back" << endl;
- //   memcpy(reinterpret_cast<float *>(f), reinterpret_cast<float *>(ftemp), n_cells * sizeof(float));
 }
