0dbba3d24a843295c6a6942da218b6fd8898986f
pcklee123 <99310789+pcklee123@users.noreply.github.com>
2023-05-17 14:27:41 +0800

    a

--- include/traj_physics.h
+++ include/traj_physics.h
@@ -5,7 +5,7 @@
                       // #define cylinder //do hot rod problem
 #define Temp_e 1e7    // in Kelvin
 #define Temp_d 1e7    // in Kelvin
-constexpr int f1 = 16; // make bigger to make smaller time steps // 8 is min for sphere slight increas in KE
+constexpr int f1 = 32; // make bigger to make smaller time steps // 8 is min for sphere slight increas in KE
 constexpr int f2 = f1 * 2;
 constexpr float incf = 1.2f;        // increment
 constexpr float decf = 1.0f / incf; // decrement factor
@@ -39,7 +39,7 @@ constexpr int n_output_part = (n_partd > 9369) ? 9369 : n_partd; // maximum numb
 // const int nprtd=floor(n_partd/n_output_part);
 
 constexpr int ndatapoints = 300; // total number of time steps to calculate
-constexpr int nc = 1;            // f1 * 1;      // number of times to calculate E and B between printouts
+constexpr int nc = 32;            // f1 * 1;      // number of times to calculate E and B between printouts
 constexpr int md_me = 60;        // ratio of electron speed/deuteron speed at the same KE. Used to calculate electron motion more often than deuteron motion
 
 #define Hist_n 1024
