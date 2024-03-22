aeaec042bdf96cdd173f1adef4ae2949aa1e51a4
John Doe <johndoe@example.com>
2024-03-22 08:17:25 +0800

    a

--- include/traj_physics.h
+++ include/traj_physics.h
@@ -1,6 +1,6 @@
 #define RamDisk // whether to use RamDisk if no ramdisk files will be in temp directory
 #define maxcells 32
-#define cldevice 1 // 0 usually means integrated GPU
+#define cldevice 0 // 0 usually means integrated GPU
 #define sphere        // do hot spot  problem
 //#define cylinder //do hot rod problem
 #define Temp_e 1e7    // in Kelvin
